6d875651610b8aa96df695d750a359bef8f90798
Isbat Ullah <ullah.isbat@gmail.com>
2023-07-09 15:17:25 -0400

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;1,300&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://raw.githubusercontent.com/isbatullah/chatbox/main/assets/css/chat.css">
-        <link rel="stylesheet" href="https://https://raw.githubusercontent.com/isbatullah/chatbox/main/assets/css/style.css">
+        <link rel="stylesheet" href="https://raw.githubusercontent.com/isbatullah/chatbox/main/assets/css/style.css">
         <link rel="stylesheet" href="https://raw.githubusercontent.com/isbatullah/chatbox/main/assets/css/typing.css">
         <title>Chatbox</title>
     </head>
